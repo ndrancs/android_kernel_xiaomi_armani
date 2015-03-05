9875d8820e05747c023110b03390a1a931262701
LuffyXDA <beastdragon01@gmil.com>
2015-03-06 01:59:17 +0530

    adapt governors for armani
    Signed-off-by: LuffyXDA <beastdragon01@gmil.com>

--- drivers/cpufreq/cpufreq_ondemandplus.c
+++ drivers/cpufreq/cpufreq_ondemandplus.c
@@ -95,25 +95,25 @@ static unsigned long up_threshold;
 #define DEFAULT_DOWN_DIFFERENTIAL 20
 static unsigned long down_differential;
 
-#define DEFAULT_MIN_FREQ 300000
+#define DEFAULT_MIN_FREQ 192000
 static u64 allowed_min;
 
-#define DEFAULT_MAX_FREQ 2265600
+#define DEFAULT_MAX_FREQ 1593600
 static u64 allowed_max;
 
-#define DEFAULT_INTER_HIFREQ 1728000
+#define DEFAULT_INTER_HIFREQ 1305600
 static u64 inter_hifreq;
 
 #define DEFAULT_INTER_LOFREQ 300000
 static u64 inter_lofreq;
 
-#define SUSPEND_FREQ 300000
+#define SUSPEND_FREQ 384000
 static u64 suspend_frequency;
 
 #define DEFAULT_INTER_STAYCYCLES 2
 static unsigned long inter_staycycles;
 
-#define DEFAULT_STAYCYCLES_RESETFREQ 652800
+#define DEFAULT_STAYCYCLES_RESETFREQ 787200
 static u64 staycycles_resetfreq;
 
 #define DEFAULT_IO_IS_BUSY 0
